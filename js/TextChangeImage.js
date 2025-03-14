712975f88184b763074bc346191ef2fe57bc10f1
ryotama63 <133328021+ryotama63@users.noreply.github.com>
2025-03-15 05:40:40 +0900

    Update TextChangeImage.js

--- js/TextChangeImage.js
+++ js/TextChangeImage.js
@@ -1,16 +1,17 @@
 (function(){
 const input = document.getElementById('input'); // 変数『input』に対象input要素を格納
 let replaceMap = {
-'0':'<img src="★★★">',
-'1':'ｂと置き換える文字',
-'2':'ｃと置き換える文字',
-'3':'Ａと置き換える文字',
-'4':'Ｂと置き換える文字',
-'5':'ａと置き換える文字',
-'6':'ｂと置き換える文字',
-'7':'ｃと置き換える文字',
-'8':'Ａと置き換える文字',
-'9':'Ｂと置き換える文字'
+'0':'<img src="./image/0.png">',
+'1':'<img src="./image/1.png">',
+'2':'<img src="./image/2.png">',
+'3':'<img src="./image/3.png">',
+'4':'<img src="./image/4.png">',
+'5':'<img src="./image/5.png">',
+'6':'<img src="./image/6.png">',
+'7':'<img src="./image/7.png">',
+'8':'<img src="./image/8.png">',
+'9':'<img src="./image/9.png">',
+'9':'<img src="./image/colon.png">'
 }
 input.onchange = () =>{
 input.value = input.value.replace(/[a-zA-Z]/g,c=>replaceMap[c]||c)
